b141b150d095b8e4c22324892a6aafb739ebad99
Alex Warstadt <alexwarstadt@gmail.com>
2020-11-11 00:59:24 -0500

    make parsing script write fewer files

--- trigger_filters/lib/parse_coca_file.py
+++ trigger_filters/lib/parse_coca_file.py
@@ -25,19 +25,18 @@ def main():
 
     base_name = os.path.basename(args.input).replace(".txt", "")
     with open(args.input, 'r') as corpus_f:
-        for line in corpus_f:
-            if EMPTY_LINE_PATTERN.match(line):
-                continue
-            coca_doc = COCADoc(line)
-            doc_id = coca_doc.doc_id.replace("@@", "")
-            for segment_no, segment in enumerate(coca_doc.segments):
-
-                out_file_name = f"{base_name}-{doc_id}-{segment_no+1}.conll"
-                out_file_path = os.path.join(args.output_dir, out_file_name)
-
-                # write each segment to it's own output file
-                # (should make trigger extraction easier)
-                with open(out_file_path, "w") as out_f:
+        out_file_name = f"{base_name}.conll"
+        out_file_path = os.path.join(args.output_dir, out_file_name)
+        with open(out_file_path, "w") as out_f:
+            for line in corpus_f:
+                if EMPTY_LINE_PATTERN.match(line):
+                    continue
+                try:
+                    coca_doc = COCADoc(line)
+                except AttributeError:
+                    print(line)
+                    continue
+                for segment_no, segment in enumerate(coca_doc.segments):
                     for speaker, turn_content in segment.turns:
                         turn_content = turn_content.strip()
                         if len(turn_content) < 1:

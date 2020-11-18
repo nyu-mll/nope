34f048ce02f11225efe2f763dd7910f01a520c51
Alex Warstadt <alexwarstadt@gmail.com>
2020-11-18 11:15:59 -0500

    finish generating factives, embedded questions

--- trigger_filters/factives.py
+++ trigger_filters/factives.py
@@ -1,22 +1,16 @@
 import spacy
 from pragmatics_dataset.trigger_filters.spacy_utils import get_dependents_string
-import re
-import numpy as np
-from pragmatics_dataset.negated_nli.negate_sentences import negate_main_verb
+from pragmatics_dataset.trigger_filters.lib.conll_reader import CoNLLReader
+from console_progressbar import ProgressBar
 
-factives = [l.strip() for l in open("wordlists/factives.jsonl")]
-
-file = open("../COCA_sample_10MB.txt")
-output = open("outputs/factives.jsonl", "w")
-
-nlp = spacy.load("en_core_web_sm")
-
-context_buffer = ["", ""]
+factives = [l.strip() for l in open("wordlists/factives.txt")]
 
 def check_sentence_for_factives(sentence):
     factives_in_sentence = [t for t in sentence if t.lemma_ in factives]
     if len(factives_in_sentence) > 0:
         for factive_verb in factives_in_sentence:
+            if check_sentence_for_quote(factive_verb):
+                continue
             comp_children = [c for c in factive_verb.children if c.dep_ == "ccomp"]
             if len(comp_children) > 0:
                 comp_clause = get_dependents_string(comp_children[0])
@@ -24,23 +18,52 @@ def check_sentence_for_factives(sentence):
     return False, None, None
 
 
-def check_sentence_for_quote(sentence, factive_verb, comp_clause):
-    pass
+def check_sentence_for_quote(factive_verb):
+    children = [t.text for t in factive_verb.children]
+    return "\"" in children or "\'" in children
 
 
-for line in file:
-    doc = nlp(line)
-    for sentence in doc.sents:
+def main():
+    output = open("outputs/factives.jsonl", "w")
+    corpus_path = "/Users/alexwarstadt/Workspace/data/COCA/parsed_0.05.conll"
+
+    n_sentences = 0
+    for line in open(corpus_path):
+        if line == "\n":
+            n_sentences += 1
+    print(n_sentences)
+    pb = ProgressBar(total=n_sentences, prefix='Here', suffix='Now', decimals=3, length=50, fill='X', zfill='-')
+
+    progress = 0
+    context_buffer = [(None, ""), (None, "")]
+    prev_segment_name = ""
+    for sentence, metadata in CoNLLReader(corpus_path):
+
+        # check if the segment changed and reset context buffer
+        if prev_segment_name != metadata["segment_id"]:
+            context_buffer = [(None, ""), (None, "")]
+            prev_segment_name = metadata["segment_id"]
+        speaker = metadata["speaker"]
         contains_factive, factive_verb, comp_clause = check_sentence_for_factives(sentence)
         if contains_factive:
             example = {
-                "sentence": sentence.text.strip(),
-                "context1": context_buffer[0].strip(),
-                "context2": context_buffer[1].strip(),
+                "context1_speaker": context_buffer[0][0],
+                "context1": str(context_buffer[0][1]),
+                "context2_speaker": context_buffer[1][0],
+                "context2": str(context_buffer[1][1]),
+                "sentence": str(sentence),
+                "speaker": speaker,
                 "factive": factive_verb.text,
                 "embedded_clause": comp_clause
             }
             output.write(str(example) + "\n")
             output.flush()
+
+        # update context buffer
         context_buffer.pop(0)
-        context_buffer.append(sentence.text)
+        context_buffer.append((speaker, sentence))
+
+        progress += 1
+        pb.print_progress_bar(progress)
+
+main()
--- trigger_filters/lib/jsonl_to_csv.py
+++ trigger_filters/lib/jsonl_to_csv.py
@@ -2,17 +2,58 @@ import sys
 from random import shuffle
 import csv
 
-input_f = open(sys.argv[1], mode='r')
-num_examples = int(sys.argv[2])
 
-examples = [eval(line) for line in input_f]
-shuffle(examples)
-with open(".".join(sys.argv[1].split(".")[:-1] + ["tsv"]), "w") as output_f:
-    header = examples[0].keys()
-    cw = csv.DictWriter(output_f, header, delimiter='\t')
-    cw.writeheader()
-    cw.writerows(examples[:num_examples])
-    # output_f.write("\t".join(examples[0].keys()) + "\r\n")
-    # for e in examples[:num_examples]:
-    #     print(len(e.values()))
-    #     output_f.write("\t".join(e.values()) + "\r\n")
+triggers = [
+            "clefts",
+            "comparatives",
+            "continuation_of_state",
+            "embedded_question",
+            "factives",
+            "implicative_predicates",
+            "re_verbs",
+            # "temporal_adverbs"
+            ]
+header = ["trigger", "context1_speaker", "context1", "context2_speaker", "context2", "sentence", "trigger_data", "speaker",
+          "appropriate?", "negatable?", "negated_sentence", "presupposition",
+          "Pr(H|C)", "Pr(H|C+P)", "Pr(H|C+~P)", "notes"]
+num_examples = 120
+people = ["alex", "alicia", "omar", "sebastian", "soo-hwan", "zhuoye"]
+outputs = {}
+for p in people:
+    f = open(f"../annotation/{p}.tsv", "w")
+    outputs[p] = csv.DictWriter(f, header, delimiter='\t')
+    outputs[p].writeheader()
+
+def standardize_dict(example, header, trigger):
+    # examples = [eval(line) for line in open(input_f)]
+    new_example = {}
+    for h in header:
+        if h in example:
+            new_example[h] = example[h]
+        else:
+            new_example[h] = ""
+    new_example["trigger_data"] = {}
+    for k in example.keys():
+        if k not in header:
+            new_example["trigger_data"][k] = example[k]
+    new_example["trigger_data"] = str(new_example["trigger_data"])
+    new_example["trigger"] = trigger
+    return new_example
+
+
+for trigger in triggers:
+    input_f = f"../outputs/{trigger}.jsonl"
+    examples = [eval(line) for line in open(input_f)]
+    shuffle(examples)
+    n = min(num_examples, len(examples))
+    examples = [standardize_dict(e, header, trigger) for e in examples[:n]]
+    indices = [int(i * n/6) for i in range(7)]
+    for i, p in enumerate(people):
+        outputs[p].writerows(examples[indices[i]:indices[i+1]])
+
+
+
+# with open(".".join(input_f.split(".")[:-1] + ["tsv"]), "w") as output_f:
+#     cw = csv.DictWriter(output_f, header, delimiter='\t')
+#     cw.writeheader()
+#     cw.writerows(examples[:num_examples])

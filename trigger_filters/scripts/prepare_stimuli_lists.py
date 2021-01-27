d7487893009e31d6fd76497b210d5e59e28f269c
Sebastian Schuster <sebschu@stanford.edu>
2021-01-26 20:31:55 -0500

    first version of judgment experiment

---
+++ trigger_filters/scripts/prepare_stimuli_lists.py
@@ -0,0 +1,64 @@
+import os, json, argparse, random
+from collections import Counter
+
+
+MAX_EXAMPLES_PER_TRIGGER = 20
+LIST_SIZE = 20
+FILLERS_PER_LIST = 5
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--target_file", type=str)
+    parser.add_argument("--filler_file", type=str)
+    parser.add_argument("--out_dir", type=str)
+
+    args = parser.parse_args()
+
+
+    with open(args.target_file, "r") as target_file:
+        examples = [json.loads(line) for line in target_file]
+
+    random.seed(2010)
+    random.shuffle(examples)
+
+    examples_per_trigger = {}
+    for example in examples:
+        trigger = example["trigger"]
+        if trigger not in examples_per_trigger:
+            examples_per_trigger[trigger] = []
+        examples_per_trigger[trigger].append(example)
+
+    target_examples = []
+    for trigger in examples_per_trigger.keys():
+        target_examples.extend(examples_per_trigger[trigger][0:MAX_EXAMPLES_PER_TRIGGER])
+
+    print(len(target_examples))
+
+    prompt_types = ["prior", "original", "negated"]
+
+    for example in target_examples:
+        random.shuffle(prompt_types)
+        example["prompt_type"] = [t for t in prompt_types]
+        example["type"] = "target"
+
+    random.shuffle(target_examples)
+
+    with open(args.filler_file, "r") as filler_file:
+        fillers = [json.loads(line) for line in filler_file]
+
+    for filler in fillers:
+        filler["type"] = "filler"
+
+    random.shuffle(fillers)
+
+    for i in range(int(len(target_examples)/LIST_SIZE)):
+        fname = os.path.join(args.out_dir, f"stimuli_list_{i}.js")
+        with open(fname, "w") as out_f:
+            stimuli_list = target_examples[(i * LIST_SIZE):((i+1) * LIST_SIZE)]
+            stimuli_list.extend(fillers[(i * FILLERS_PER_LIST):((i+1) * FILLERS_PER_LIST)])
+            json_str = json.dumps(stimuli_list)
+            print(f"var all_stims = {json_str};", file=out_f)
+
+
+if __name__ == '__main__':
+    main()

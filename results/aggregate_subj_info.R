9abd783e8b932c8f85b4bd605baf964433de987a
Alicia Parrish <alicia.v.parrish@gmail.com>
2021-02-05 17:29:40 -0500

    update data n = 21

--- results/aggregate_subj_info.R
+++ results/aggregate_subj_info.R
@@ -1,8 +1,15 @@
 library(tidyverse)
 
-setwd("C:/Users/NYUCM Loaner Access/Documents/GitHub/presupposition_dataset/results")
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
 
-dat = read.csv("01_prescreener/subj_info.csv")
+######################################################
+#
+# PRESCREENER
+#
+######################################################
+
+dat = read.csv("./01_prescreener/subj_info.csv")
 
 dat2 = dat %>%
   gather("Question","Value", -anon_id)
@@ -13,6 +20,39 @@ dat3 <- dat2 %>%
   filter(Value != -1)
 
 plt = ggplot(data=dat3, aes(x=Value))+
+  geom_bar(stat = 'count')+
+  facet_wrap(~Question,scales = "free_x")
+plt
+
+######################################################
+#
+# JUDGMENTS
+#
+######################################################
+
+trials = read.csv("./02_judgments/trials.csv")
+subj_info = read.csv("./02_judgments/subj_info.csv")
+
+# check times
+min(trials$Answer.time_in_minutes)
+max(trials$Answer.time_in_minutes)
+mean(trials$Answer.time_in_minutes)
+median(trials$Answer.time_in_minutes)
+sort(unique(trials$Answer.time_in_minutes))
+
+# check how many responses from different participants
+trials %>% group_by(anon_id) %>% summarise(count=n()/25)
+
+# look at the other rating info given by participants
+subj_info2 = subj_info %>%
+  gather("Question","Value", -anon_id)
+
+plot_values = c("enjoyment", "asses", "fairprice")
+subj_info3 <- subj_info2 %>% 
+  filter(Question %in% plot_values) %>%
+  filter(Value != -1)
+
+plt = ggplot(data=subj_info3, aes(x=Value))+
   geom_bar(stat = 'count')+
   facet_wrap(~Question,scales = "free_x")
 plt

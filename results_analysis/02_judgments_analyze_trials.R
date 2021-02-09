82b0314cf9d14c86d36d9346ddcc5ad47df8d94c
Alicia Parrish <alicia.v.parrish@gmail.com>
2021-02-09 14:10:12 -0500

    make line plots

--- results_analysis/02_judgments_analyze_trials.R
+++ results_analysis/02_judgments_analyze_trials.R
@@ -9,8 +9,7 @@ dat = read.csv("../results/02_judgments/trials.csv")
 stims = read.csv("../experiments/stimuli/all_annotations_cleaned.csv")
 stims = stims %>% select(sent_id,trigger) %>% rename("id" = sent_id)
 
-###################################
-# LOOK AT THE FILLER ITEMS
+# ----------------- LOOK AT THE FILLER ITEMS -----------------
 
 dat2 <- dat %>%
   filter(type=="filler")%>%
@@ -43,9 +42,10 @@ dat2_corr <- dat2 %>%
            axis.ticks.x=element_blank())+
      facet_wrap(~expected_resp,scales="free_x"))
 
-###################################
-# LOOK AT THE TARGET ITEMS
 
+# ----------------- LOOK AT THE TARGET ITEMS -----------------
+
+# PLOT ALL CONDITIONS WITH SCATTERPLOTS AND BOXPLOTS
 dat3 <- dat %>%
   filter(type!="filler")%>%
   group_by(id,type)%>%
@@ -57,11 +57,31 @@ plt2 = ggplot(data=dat4, aes(x=type,y=mean_resp,col=type))+
   geom_jitter()+
   geom_boxplot(alpha=0)+
   facet_wrap(~trigger)+
-  ggtitle("Judgments pilot, partial data with 137 responses")+
+  ggtitle("Judgments pilot, partial data with 147 responses")+
   theme(plot.title = element_text(hjust = 0.5),
         axis.title.x=element_blank(),
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank(),
         legend.position = c(0.9, 0.2))
 plt2
+
+# MAKE LINE PLOTS SHOWING PRIOR TO POSTERIOR
+dat5<-dat4%>%
+  spread(type,mean_resp)%>%
+  gather(condition,posterior,-id,-trigger,-`target-prior`)%>%
+  rename("prior"=`target-prior`)%>%
+  gather(rating_type,response,-id,-trigger,-condition)
+
+dat5$rating_type <- factor(dat5$rating_type, levels = c("prior","posterior"))
+dat5$condition <- factor(dat5$condition, levels = c("target-original","target-negated"))
+(plt3 <- ggplot(data=dat5,aes(x=rating_type,y=response,group=id))+
+  geom_line()+
+  facet_grid(condition~trigger)+
+  scale_x_discrete(expand=c(0,0.05))+
+  ggtitle("Judgments pilot, partial data with 147 responses (lines show prior to posterior judgment)")+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+)
   

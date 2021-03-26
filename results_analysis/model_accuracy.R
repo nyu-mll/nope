c897098aefde374584e76b6e0e73d28b43c94928
Alicia Parrish <alicia.v.parrish@gmail.com>
2021-03-26 14:09:26 -0400

    analyze adversarial examples we wrote

--- results_analysis/model_accuracy.R
+++ results_analysis/model_accuracy.R
@@ -28,7 +28,9 @@ N_resp = mean(dat2$mean_resp[dat2$pred2=="N"])
    geom_hline(yintercept=N_resp, color="blue")+
    ggtitle("roberta large mnli by participant mean response")+
    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
-         plot.title = element_text(hjust = 0.5)))
+         plot.title = element_text(hjust = 0.5))#+
+    #facet_wrap(~type)
+  )
 
 # dat3 <- dat2 %>%
 #   mutate(diff = case_when(pred2=="C" ~ mean_resp - C_resp,
@@ -54,15 +56,121 @@ for(i in 1:nrow(dat2)){
   s2=dat2$hypothesis[i]
   ss2=unique(unlist(strsplit(gsub("\\.","",tolower(s2))," ")))
   o=intersect(ss,ss2)
-  dat3$overlap[i]=(length(o)/length(ss2))*100
+  dat3$overlap[i]=(length(o)/length(ss2))*100  # overlap = % of words in hyp that are also in premise
 }
 
 (plt3 = ggplot(dat3,aes(x=overlap,col=pred2))+
     geom_freqpoly(binwidth = 10,size=1.2)+
     #scale_y_continuous(trans = 'log10')+
+    #facet_grid(.~trigger)+
+    xlim(c(0,100))+
+    xlab("overlap rate")+
+    ggtitle("Overlap rates")+
+    theme(plot.title = element_text(hjust = 0.5))
+)
+
+
+#----------------  OVERLAP RATE COMPARISON BETWEEN ORIGINAL AND ADVERSARIAL ---------------- 
+
+dat_adversarial = read.csv("../annotations/03_pressupposition_dataset_adversarial_annotations.csv")
+
+dat_adv2 <- dat_adversarial %>%
+  mutate(full_input_pos = ifelse(altered.sentence!="", paste0(context1," ", context2," ",altered.sentence), paste0(context1," ",context2," ",sentence))) %>%
+  mutate(full_input_neg = paste0(context1," ", context2," ",negated_sentence))%>%
+  select(sent_id,trigger,full_input_pos,full_input_neg, presupposition, adversarial_hypothesis)%>%
+  gather(condition,context,-sent_id,-trigger,-presupposition,-adversarial_hypothesis)%>%
+  mutate(condition=case_when(condition=="full_input_pos" ~ "pos",
+                             condition=="full_input_neg" ~ "neg"))%>%
+  filter(!adversarial_hypothesis=="")
+
+dat_adv2$overlap_orig = NA
+dat_adv2$overlap_adv= NA
+
+for(i in 1:nrow(dat_adv2)){
+  s=dat_adv2$context[i]
+  ss=unique(unlist(strsplit(gsub("\\.","",tolower(s))," ")))
+  s2=dat_adv2$presupposition[i]
+  ss2=unique(unlist(strsplit(gsub("\\.","",tolower(s2))," ")))
+  o=intersect(ss,ss2)
+  dat_adv2$overlap_orig[i]=(length(o)/length(ss2))*100  # overlap = % of words in hyp that are also in premise
+  
+  s3=dat_adv2$adversarial_hypothesis[i]
+  ss3=unique(unlist(strsplit(gsub("\\.","",tolower(s3))," ")))
+  o2=intersect(ss,ss3)
+  dat_adv2$overlap_adv[i]=(length(o2)/length(ss3))*100
+}
+
+dat_adv3 <- dat_adv2 %>%
+  group_by(trigger,condition)%>%
+  summarise(overlap_orig = mean(overlap_orig),
+            overlap_adv = mean(overlap_adv))%>%
+  mutate(diff = overlap_adv - overlap_orig)
+
+write.csv(dat_adv2,"../annotations/03_adversarial_examples.csv")
+
+
+# ---------------- ANALYZE ADVERSARIAL PREDS -----------------
+
+dat = read.csv("../model_results/03_preds_adversarial_roberta_mnli.csv",stringsAsFactors = F)
+
+dat2 <- dat %>%
+  mutate(preds_orig = case_when(preds_orig=='tensor(0)' ~ "C",
+                                preds_orig=='tensor(1)' ~ "N",
+                                preds_orig=='tensor(2)' ~ "E"))%>%
+  mutate(preds_adv = case_when(preds_adv=='tensor(0)' ~ "C",
+                               preds_adv=='tensor(1)' ~ "N",
+                               preds_adv=='tensor(2)' ~ "E"))%>%
+  select(-X,-Unnamed..0)
+
+dat_advers2<-dat_adversarial%>%
+  filter(adversarial_hypothesis!='')%>%
+  select(sent_id,expected_relation_negated,expected_relation_original)%>%
+  gather(condition,expected_pred,-sent_id)%>%
+  mutate(condition=case_when(condition=="expected_relation_negated" ~ "neg",
+                             condition=="expected_relation_original" ~ "pos"))
+
+dats<-merge(dat2,dat_advers2,by=c("sent_id","condition"))
+
+dats2 <- dats %>%
+  mutate(corr_orig = case_when(preds_orig=="E" ~ 1,
+                               preds_orig!="E" ~ 0)) %>%
+  mutate(corr_adv = case_when(preds_adv==expected_pred ~ 1,
+                              preds_adv!=expected_pred ~ 0)) %>%
+  mutate(corr_adv_nonentail = case_when(preds_adv=="E" ~ 0,
+                                   preds_adv!="E" ~ 1))%>%
+  mutate(entailment_both_preds = ifelse(preds_orig=="E" & preds_adv == "E",1,0))%>%
+  mutate(same_response = ifelse(preds_orig==preds_adv,1,0))
+
+# get means
+dats3<-dats2 %>%
+  group_by(trigger,condition)%>%
+  summarise(corr_orig = mean(corr_orig),
+            corr_adv = mean(corr_adv),
+            corr_adv_nonentail = mean(corr_adv_nonentail),
+            entailment_both_preds = mean(entailment_both_preds),
+            same_response = mean(same_response))
+
+dats_compare_resp<-dats2 %>%
+  select(sent_id,trigger,condition,preds_orig,preds_adv)%>%
+  gather(hypothesis_type,pred,-sent_id,-trigger,-condition)%>%
+  mutate(pred = case_when(pred=="E" ~ "entailment",
+                          pred=="N"|pred=="C" ~ "non-entailment"))
+
+# plot accuracy to compare original and adversarial examples
+(plt<-ggplot(data=dats_compare_resp, aes(x=trigger,fill=pred))+
+  geom_bar(stat='count')+
+  facet_grid(hypothesis_type~condition)+
+  ggtitle("roberta large mnli predictions")+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+          plot.title = element_text(hjust = 0.5)))
+
+# plot overlap rate for adversarial cases
+(plt2 = ggplot(dats2,aes(x=overlap_adv,col=preds_adv))+
+    geom_freqpoly(binwidth = 10,size=1.2)+
+    #scale_y_continuous(trans = 'log10')+
     facet_grid(.~trigger)+
     xlim(c(0,100))+
     xlab("overlap rate")+
-    ggtitle("Overlap rates in each protocol")+
+    ggtitle("Overlap rates in adversarial examples")+
     theme(plot.title = element_text(hjust = 0.5))
 )

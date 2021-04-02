31adfb63bf8ab24a58e968758b8a5f826c290d4d
Sebastian Schuster <sebschu@stanford.edu>
2021-04-02 12:11:58 -0400

    prepare experiment for re-annotation

--- trigger_filters/scripts/cleaned_csv_to_jsonl.py
+++ trigger_filters/scripts/cleaned_csv_to_jsonl.py
@@ -7,8 +7,19 @@ import json
 with open(sys.argv[1], "r") as csv_f, open(sys.argv[2], "w") as out_f:
   reader = csv.DictReader(csv_f)
   for line in reader:
-    if len(line["presupposition_corrected"].strip()) > 0:
-      line["presupposition"] = line["presupposition_corrected"]
+    if "presupposition_corrected" in line:
+      if len(line["presupposition_corrected"].strip()) > 0:
+        line["presupposition"] = line["presupposition_corrected"]
     
-    del line["presupposition_corrected"]
+      del line["presupposition_corrected"]
+    
+    
+    if "sentence with restriction" in line:
+      line["sentence"] = line["sentence with restriction"]
+      line["negated_sentence"] = line["negated sentence with restriction"]
+      line["presupposition"] = line["presupposition with restriction"]
+      del line["sentence with restriction"]
+      del line["negated sentence with restriction"]
+      del line["presupposition with restriction"]
+    
     print(json.dumps(line), file=out_f)

48b37712546d4441cb7d5f47b9e0eddb4213919d
Sebastian Schuster <sebschu@stanford.edu>
2021-04-26 12:16:36 -0400

    round2 of experiments

--- trigger_filters/scripts/cleaned_csv_to_jsonl.py
+++ trigger_filters/scripts/cleaned_csv_to_jsonl.py
@@ -7,6 +7,37 @@ import json
 with open(sys.argv[1], "r") as csv_f, open(sys.argv[2], "w") as out_f:
   reader = csv.DictReader(csv_f)
   for line in reader:
+    if "Appropriate_2 (Y/N)" in line:
+      if line["Appropriate_2 (Y/N)"].lower() == "n":
+        continue
+      else:
+        line["appropriate?"] = line["Appropriate_2 (Y/N)"]
+        del line["Appropriate_2 (Y/N)"]
+    
+    if "numeric_determiners_new" in line:
+      if line["numeric_determiners_new"].lower() == "false":
+        continue
+      else:
+        del line["numeric_determiners_new"]
+    
+    
+    if "negated_sentence_2" in line:
+      if len(line["negated_sentence_2"].strip()) > 0:
+        line["negated_sentence"] = line["negated_sentence_2"]
+      del line["negated_sentence_2"]
+    
+    if "presupposition_2" in line:
+      if len(line["presupposition_2"].strip()) > 0:
+        line["presupposition"] = line["presupposition_2"]
+      del line["presupposition_2"]
+    
+    if "adversarial_hypothesis" in line:
+      line["presupposition"] = line["adversarial_hypothesis"]
+      del line["adversarial_hypothesis"]
+      
+    
+    
+    
     if "presupposition_corrected" in line:
       if len(line["presupposition_corrected"].strip()) > 0:
         line["presupposition"] = line["presupposition_corrected"]
--- trigger_filters/scripts/mnli_fillers.py
+++ trigger_filters/scripts/mnli_fillers.py
@@ -1,12 +1,12 @@
 import random
 import jsonlines
 
-matched = "/Users/alexwarstadt/workspace/data/multinli_1.0/multinli_1.0_dev_matched.jsonl"
-mismatched = "/Users/alexwarstadt/workspace/data/multinli_1.0/multinli_1.0_dev_mismatched.jsonl"
+matched = "/Users/sebschu/Downloads/multinli_1.0/multinli_1.0_dev_matched.jsonl"
+mismatched = "/Users/sebschu/Downloads/multinli_1.0/multinli_1.0_dev_mismatched.jsonl"
 
 mnli = [eval(l) for l in open(matched)] + [eval(l) for l in open(mismatched)]
 all_entail = ["entailment"] * 5
-mnli = [l for l in mnli if l["annotator_labels"] == ["entailment"] * 5 or l["annotator_labels"] == ["contradiction"] * 5]
+mnli = [l for l in mnli if l["annotator_labels"] == ["entailment"] * 5 or l["annotator_labels"] == ["contradiction"] * 5 or l["annotator_labels"] == ["neutral"] * 5]
 
 random.shuffle(mnli)
 sample = mnli[:1500]
@@ -21,7 +21,7 @@ for line in sample:
         "sent_id": line["pairID"] + "_filler"
     })
 
-with open("../../experiments/stimuli/mnli_fillers.jsonl", "w") as output:
+with open("../../experiments/stimuli/mnli_fillers_2.jsonl", "w") as output:
     writer = jsonlines.Writer(output)
     writer.write_all(fillers)
 

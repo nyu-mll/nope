ff718cf1639c2deb851511c74f11b7778efd077d
Alicia Parrish <alicia.v.parrish@gmail.com>
2021-05-29 16:14:41 -0400

    Update IAA.R

--- results_analysis/IAA.R
+++ results_analysis/IAA.R
@@ -1,8 +1,11 @@
 library(irr)
+library(boot)
 library(tidyverse)
 
 this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(this.dir)
+
+set.seed(42)
 
 trials = read.csv("../annotated_corpus/main_corpus.individual_ratings.csv",stringsAsFactors = F)
 
@@ -22,32 +25,31 @@ trials_sep <- trials %>%
 trials2 <- merge(trials_sep,stims,by="id")
 
 # which items are adversarial
-trials2 <- trials2 %>% unite("trigger",trigger,adversarial, sep="_")
+#trials2 <- trials2 %>% unite("trigger",trigger,adversarial, sep="_")
 
 # ---------------------------------------------------
 # loop through and get alpha values for each trigger & type
 
 triggers = unique(trials2$trigger)
 types = unique(trials2$type)
+adv = unique(trials2$adversarial)
+
+# look at non-adversarial only
+trials3 <- filter(trials2, adversarial == "non")
 
 data = data.frame(matrix(ncol = length(types), nrow = length(triggers)))
 colnames(data) = types
 rownames(data) = triggers
 
-# can get rid of later, but right now can't run these because of duplicated values
-cant_run = c("embedded_question_non","implicative_predicates_non","implicative_predicates_non",
-             "re_verbs_non","temporal_adverbs_adv","clefts_non")
-triggers = triggers[!triggers %in% cant_run]
-
 for(tr in triggers){
   for(ty in types){
-    trials3<-trials2 %>%
+    trials4<-trials3 %>%
       filter(type == ty)%>%
       filter(trigger == tr) %>%
       select(id,response,anon_id)%>%
       spread(id,response)
-    t3<- data.matrix(trials3)
-    k=kripp.alpha(t3,"interval")
+    tt<- data.matrix(trials4)
+    k=kripp.alpha(tt,"interval")
     data[as.character(tr),as.character(ty)] <- k$value
   }
 }
@@ -58,7 +60,66 @@ View(data)
 # ---------------------------------------------------
 # bootstrapping for distribution & CIs
 
+par(mar=c(2,2,2,2))
+par(mfrow = c(length(triggers), 2))
 
+n <- length(triggers) * length(types)
+list_plot <- vector(mode = "list", length = n)
+names(list_plot) <- paste("plot", 1:n)
+
+boots<-vector(mode = "list", length = n)
+ci_lows<-vector(mode = "list", length = n)
+ci_his<-vector(mode = "list", length = n)
+
+for(i in 1:length(triggers)){
+  for(j in 1:length(types)){
+    dat <- trials3 %>%
+      filter(trigger == triggers[i],
+             type == types[j])%>%
+      select(id,response,anon_id)%>%
+      spread(id,response)
+    tt<- data.matrix(dat)
+
+    #kripp.alpha(tt,"interval") # calculate fleiss's kappa
+    
+    bt <- boot(tt, function(x, idx) {kripp.alpha(x[,idx], "interval")$value}, R=1000)
+    
+    boots[[((i-1)*2)+j]] <- bt
+    ci<-boot.ci(bt)
+    ci_lows[[((i-1)*2)+j]]<-as.list(strsplit(as.character(unlist(ci[4])), '\\s+')[[2]])
+    ci_his[[((i-1)*2)+j]]<-as.list(strsplit(as.character(unlist(ci[4])), '\\s+')[[3]])
+    #list_plot[[((i-1)*2)+j]] <-hist(bt$t, freq = FALSE, breaks = 50)
+    #names(list_plot)[((i-1)*2)+j] = paste0(triggers[i],": ",types[j])
+    
+    print(paste0("finished ", triggers[i], " for ", types[j]))
+  }
+}
+save(boots, file="Rdata/boots_combined.RData")
+save(ci_his, file="Rdata/ci_his_combined.RData")
+save(ci_lows, file="Rdata/ci_lows_combined.RData")
+
+# organize data for plotting
+dat_boots = NULL
+for(i in c(1:n)){
+  temp_list = boots[[i]]
+  tr_index = ceiling(i/2)
+  ty_index = ((i+1) %% 2) + 1
+  temp_dat = data.frame("trigger" = c(rep(as.character(triggers[tr_index]),1000)),
+                        "type" = c(rep(types[ty_index],1000)),
+                        "val" = temp_list$t)
+  dat_boots = rbind(dat_boots, temp_dat)
+  print(paste0(triggers[tr_index], ": ", types[ty_index]))
+}
+
+# plotting
+(plt<-ggplot(data=dat_boots, aes(x=val))+
+  geom_histogram(binwidth = 0.01)+
+  facet_grid(trigger~type))
+
+
+
+#########################################################
+#                        old
 
 
 
@@ -85,8 +146,8 @@ for(ty in unique(fillers$expected_resp)){
 
 
 # ----------GET OVERALL VALUES ---------- 
-for(ty in unique(trials$type)){
-  trials5<-trials %>%
+for(ty in unique(trials3$type)){
+  trials5<-trials3 %>%
     filter(type == ty)%>%
     select(id,response,anon_id)%>%
     spread(id,response)
@@ -98,7 +159,7 @@ for(ty in unique(trials$type)){
 
 
 # ---------- OVERALL OVERALL ---------- 
-trials6 <- trials %>%
+trials6 <- trials3 %>%
   unite('ids',c(id,type))%>%
   select(ids,response,anon_id)%>%
   spread(ids,response)

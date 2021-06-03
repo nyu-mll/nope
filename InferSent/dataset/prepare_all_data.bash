d00e550d1259f4cd62e4c050a1f388b36ae48129
Alex Warstadt <alexwarstadt@gmail.com>
2021-06-03 16:18:00 -0400

    remove FEVER from test/dev of combined datasets

--- InferSent/dataset/prepare_all_data.bash
+++ InferSent/dataset/prepare_all_data.bash
@@ -4,10 +4,10 @@ rm build.tgz\?dl\=0
 mv data/build/* .
 rm -rf data
 
-# call python script to combine all train/test/dev sets from 3 SNLI/MNLI/ANLI
-python3 combine_datasets.py
-
-# python script to process mnli data
-python3 process_mnli.py
-rm mnli/m_dev.jsonl mnli/mm_dev.jsonl
-rm -rf snli anli fever_nli
+## call python script to combine all train/test/dev sets from 3 SNLI/MNLI/ANLI
+#python3 combine_datasets.py
+#
+## python script to process mnli data
+#python3 process_mnli.py
+##rm mnli/m_dev.jsonl mnli/mm_dev.jsonl
+#rm -rf snli anli fever_nli

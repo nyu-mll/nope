035c74ddaa43d3ab3caec3cb64bc609dca6123f6
Alex Warstadt <alexwarstadt@gmail.com>
2021-06-04 13:24:47 -0400

    add loop to evaluate script to test all models

--- InferSent/evaluate_script.py
+++ InferSent/evaluate_script.py
@@ -1,6 +1,6 @@
 from models import NLINet
 import torch
-from data import get_nli, get_batch, build_vocab
+from data import build_vocab, get_batch
 import argparse
 import numpy as np
 import pandas as pd
@@ -8,95 +8,114 @@ import os
 
 parser = argparse.ArgumentParser(description='NLI training')
 # paths
-parser.add_argument("--nlipath", type=str, default='dataset/SNLI/', help="NLI data path (SNLI or MultiNLI)")
-parser.add_argument("--outputdir", type=str, default='savedir/', help="Output directory")
+parser.add_argument("--eval_data_path", type=str, default='dataset/combined/', help=".jsonl file with eval data. Required fields: premise, hypothesis, label. ")
+parser.add_argument("--output_dir", type=str, default='results/', help="Output directory")
+parser.add_argument("--output_name", type=str, help="Name of eval data")
 parser.add_argument("--word_emb_path", type=str, default="dataset/fastText/crawl-300d-2M.vec", help="word embedding file path")
 parser.add_argument("--use_cuda", type=int, default=1)
+parser.add_argument("--model_dir", type=str, default="final_models", help="path to directory of directories containing models to eval")
+parser.add_argument("--do_tokenize", type=int, default=1, help="Whether or not to tokenize")
+parser.add_argument("--concise_output", type=int, default=0, help="When 1, don't write all results to separate files. Just one dataframe.")
 
 args = parser.parse_args()
 
-def load_model(model_path):
-    params_model = get_params(model_path)
-    nli_net = NLINet(params_model)
-    if args.use_cuda:
-        nli_net.load_state_dict(torch.load(model_path))
-    else:
-        nli_net.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-    if args.use_cuda:
-        nli_net.classifier.to("cuda:0")
-    return nli_net, params_model
+### PROCESS EVAL DATA ###
+if args.do_tokenize:
+    from nltk.tokenize import word_tokenize
+df_eval_data = pd.read_json(args.eval_data_path, orient="records", lines=True)
+word_vec = build_vocab(list(df_eval_data["hypothesis"]) + list(df_eval_data["premise"]), args.word_emb_path)
+bos, eos = ("<p>", "</p>")
+if args.do_tokenize:
+    tokenize = lambda s: ' '.join(word_tokenize(s).replace(" n't ", "n 't "))
+    df_eval_data["premise"] = df_eval_data["premise"].apply(tokenize)
+    df_eval_data["hypothesis"] = df_eval_data["hypothesis"].apply(tokenize)
+s1 = [[bos] +
+      [word for word in sent.split() if word in word_vec] +
+      [eos] for sent in df_eval_data["premise"]]
+s2 = [[bos] +
+      [word for word in sent.split() if word in word_vec] +
+      [eos] for sent in df_eval_data["hypothesis"]]
+labels = np.array(df_eval_data["label"])
 
 
-def get_params(model_path):
+all_predictions = []
+all_logits = []
+for experiment in os.listdir(args.model_dir):
+    model_type = {"bow": "BOW", "infersent": "InferSent"}[experiment.split("_")[0]]
+    training_data_type = {"mnli": "mnli", "combined": "mnli-snli-anli-fever"}[experiment.split("_")[1]]
+    if not args.concise_output:
+        os.makedirs(os.path.join(args.output_dir, model_type, training_data_type), exist_ok=True)
+    for i, model in enumerate(os.listdir(os.path.join(args.model_dir, experiment))):
+        model_path = os.path.join(args.model_dir, experiment, model)
 
-    # extract parameters from checkpoint name
-    params = model_path[:-10]
-    params = params.split("/")[-1]
-    params = {p.split("=")[0]: p.split("=")[1] for p in params.split(",")}
+        ### GET MODEL PARAMETERS ###
+        model_name = model[:-10]
+        params = {p.split("=")[0]: p.split("=")[1] for p in model_name.split(",")}
 
-    # rename/type certain parameters
-    params['bsize'] = int(params['batch_size'])
-    params['fc_dim'] = int(params['fc_dim'])
+        # rename/type certain parameters
+        params['bsize'] = int(params['batch_size'])
+        params['fc_dim'] = int(params['fc_dim'])
 
-    # add default values for required parameters
-    params['nonlinear_fc'] = 0
-    params['n_classes'] = 3
-    params['dpout_fc'] = 0
-    params['project_bow'] = 0
-    params['word_emb_dim'] = 300
-    params['use_cuda'] = bool(args.use_cuda)
+        # add default values for required parameters
+        params['nonlinear_fc'] = 0
+        params['n_classes'] = 3
+        params['dpout_fc'] = 0
+        params['project_bow'] = 0
+        params['word_emb_dim'] = 300
+        params['use_cuda'] = bool(args.use_cuda)
 
-    # modify params dict depending on encoder type
-    if "encoder_type" in params:    # i.e. if BOW was used
-        params['enc_lstm_dim'] = 1
-        for k in ['batch_size', 'jobname', 'n_restarts', 'seed']:
-            params.pop(k)
-    else: # i.e. if InferSent was used
-        params['encoder_type'] = "InferSent"
-        params['dpout_model'] = float(params['dpout_model'])
-        params['enc_lstm_dim'] = int(params['enc_lstm_dim'])
-        params['n_enc_layers'] = 1
-        params['pool_type'] = "max"
-    return params
+        # modify params dict depending on encoder type
+        if "encoder_type" in params:    # i.e. if BOW was used
+            params['enc_lstm_dim'] = 1
+            for k in ['batch_size', 'jobname', 'n_restarts', 'seed']:
+                params.pop(k)
+        else:    # i.e. if InferSent was used
+            params['encoder_type'] = "InferSent"
+            params['dpout_model'] = float(params['dpout_model'])
+            params['enc_lstm_dim'] = int(params['enc_lstm_dim'])
+            params['n_enc_layers'] = 1
+            params['pool_type'] = "max"
 
+        ### LOAD MODEL ###
+        nli_net = NLINet(params)
+        nli_net.eval()
+        if args.use_cuda:
+            nli_net.load_state_dict(torch.load(model_path))
+        else:
+            nli_net.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+        if args.use_cuda:
+            nli_net.classifier.to("cuda:0")
 
+        ### RUN EVAL DATA ###
+        preds = []
+        logits = []
+        for stidx in range(0, len(s1), params['bsize']):
+            s1_batch, s1_len = get_batch(s1[stidx:stidx + params['bsize']],
+                                         word_vec, params['word_emb_dim'])
+            s2_batch, s2_len = get_batch(s2[stidx:stidx + params['bsize']],
+                                         word_vec, params['word_emb_dim'])
 
-# eval_model("savedir/jobname=bow_combined,encoder_type=BOW,batch_size=128,fc_dim=256,pool_type=mean,n_restarts=4,dataset=combined,seed=445.final.pkl")
+            output = nli_net((s1_batch, s1_len), (s2_batch, s2_len))
+            logits.append(output.detach().numpy())
+            preds.append(np.array([['e', 'n', 'c'][int(p)] for p in output.data.max(1)[1]]))
+        preds = pd.DataFrame(index=df_eval_data.index, columns=[model_name], data=np.concatenate(preds))
+        logits = pd.DataFrame(index=df_eval_data.index, data=[{model_name: x} for x in np.concatenate(logits)])
 
-def prepare_data(eval_data_path):
-    df = pd.read_json(eval_data_path, orient="records", lines=True)
-    word_vec = build_vocab(list(df["hypothesis"]) + list(df["premise"]), args.word_emb_path)
-    bos, eos = ("<p>", "</p>")
+        ### WRITE OUTPUTS TO INDIVIDUAL FILE ###
+        if not args.concise_output:
+            out_dir = os.path.join(args.output_dir, model_type, training_data_type, str(i))
+            os.makedirs(out_dir, exist_ok=True)
 
-    s1 = [[bos] +
-          [word for word in sent.split() if word in word_vec] +
-          [eos] for sent in df["premise"]]
-    s2 = [[bos] +
-          [word for word in sent.split() if word in word_vec] +
-          [eos] for sent in df["hypothesis"]]
-    labels = np.array(df["label"])
-    return s1, s2, labels, word_vec
+            df_out = pd.concat([preds.rename({model_name: "predicted_label"}, axis=1), logits.rename({model_name: "logits"}, axis=1)], axis=1)
+            if "uid" in df_eval_data.columns:
+                df_out = pd.concat([df_eval_data[["uid"]], df_out], axis=1)
+            df_out.to_json(os.path.join(out_dir, args.output_name + ".jsonl"), orient="records", lines=True)
+        all_predictions.append(preds)
+        all_logits.append(logits)
 
-
-def evaluate_model(nli_net, s1, s2, labels, word_vec, batch_size, word_emb_dim=300):
-    preds = []
-    for stidx in range(0, len(s1), batch_size):
-        s1_batch, s1_len = get_batch(s1[stidx:stidx + batch_size],
-                                     word_vec, word_emb_dim)
-        s2_batch, s2_len = get_batch(s2[stidx:stidx + batch_size],
-                                     word_vec, word_emb_dim)
-
-        output = nli_net((s1_batch, s1_len), (s2_batch, s2_len))
-        pred = output.data.max(1)[1]
-        print(pred)
-        preds.append(pred)
-    # preds = np.append(preds)
-    return preds
-
-
-# for sweep in os.listdir("savedir"):
-
-
-nli_net, params_model = load_model("savedir/jobname=infersent_mnli,batch_size=32,dpout_model=0.0,enc_lstm_dim=1024,fc_dim=512,n_restarts=1,dataset=mnli,seed=4926")
-s1, s2, labels, word_vec = prepare_data("dataset/mnli/test.jsonl")
-evaluate_model(nli_net, s1, s2, labels, word_vec, params_model['bsize'])
+### MAKE FINAL DATAFRAME ###
+df_all_predictions = pd.concat([df_eval_data] + all_predictions, axis=1).set_index(list(df_eval_data.columns))
+df_all_logits = pd.concat([df_eval_data] + all_logits, axis=1).set_index(list(df_eval_data.columns))
+df_out = {"predicted_label": df_all_predictions, "logits": df_all_logits}
+df_out = pd.concat(df_out, axis=1).reset_index()
+df_out.to_json(os.path.join(args.output_dir, args.output_name + ".all_models.jsonl"), orient="records", lines=True, index=True, force_ascii=False)

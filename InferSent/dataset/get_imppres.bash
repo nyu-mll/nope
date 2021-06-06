264c28c1a1f657d27f204c499ec869b6df94e6d8
Alex Warstadt <alexwarstadt@gmail.com>
2021-06-06 15:16:44 -0400

    clean and comment imppres download script

--- InferSent/dataset/get_imppres.bash
+++ InferSent/dataset/get_imppres.bash
@@ -1,20 +1,22 @@
+# Script to download and preprocess imppres
+
+# Download and unzip
 rm -rf imppres
 mkdir imppres
-
 wget https://github.com/facebookresearch/Imppres/archive/refs/heads/master.zip
 unzip master.zip
 mv Imppres-master/dataset/presupposition/* imppres
 rm master.zip
 rm -rf Imppres-master
+
+# make master file with all imppres data
 cat imppres/* > imppres/imppres_all.jsonl
-sed -i '.bak' -e 's/sentence1/premise/' imppres/imppres_all.jsonl
-sed -i '.bak' -e 's/sentence2/hypothesis/' imppres/imppres_all.jsonl
-#
-#for x in all_n_presupposition both_presupposition change_of_state cleft_existence cleft_uniqueness question_presupposition
-#do
-#  wget https://raw.githubusercontent.com/facebookresearch/Imppres/master/dataset/presupposition/$x.jsonl
-#  mv $x.jsonl imppres
-##  sed -i '.bak' -e 's/sentence1/premise/' imppres/$x
-##  sed -i '.bak' -e 's/sentence2/hypothesis/' imppres/$x
-#  cat imppres/$x > imppres_all.jsonl
-#done
+
+## rename fields to comply with evaluate_script.py (for MacOS)
+#sed -i '.bak' -e 's/sentence1/premise/' imppres/imppres_all.jsonl
+#sed -i '.bak' -e 's/sentence2/hypothesis/' imppres/imppres_all.jsonl
+
+
+# rename fields to comply with evaluate_script.py (for Linux)
+sed -i 's/sentence1/premise/' imppres/imppres_all.jsonl
+sed -i 's/sentence2/hypothesis/' imppres/imppres_all.jsonl
